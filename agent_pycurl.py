768fb04228e9fd573da6fcbb468729684d723265
Richardson Lima <contatorichardsonlima@gmail.com>
2015-12-22 22:36:17 -0300

    change pycurl agent

---
+++ agent_pycurl.py
@@ -0,0 +1,12 @@
+import pycurl, json
+
+api_url = 'http://localhost:5000/api/v1.0/collector'
+
+data = json.dumps({"arquitetura": "x64", "hostname": "hostname-test", "sistema": "linux", "done": "True"})
+
+c = pycurl.Curl()
+c.setopt(pycurl.URL, api_url)
+c.setopt(pycurl.HTTPHEADER, ['X-Postmark-Server-Token: API_TOKEN_HERE','Accept: application/json'])
+c.setopt(pycurl.POST, 1)
+c.setopt(pycurl.POSTFIELDS, data)
+c.perform()
