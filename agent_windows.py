81b6b6212770b542e4547f0803c11ef94cb4a8dc
Richardson Lima <contatorichardsonlima@gmail.com>
2015-12-31 13:55:33 -0300

    changes

---
+++ agent_windows.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Richardson Lima"
+__copyright__ = "Copyright 2015, Python Infra DevOps Project"
+__credits__ = ["",
+                    ""]
+__license__ = "GNU General Public License v2.0"
+__version__ = "1.0.1"
+__maintainer__ = "Richardson Lima"
+__email__ = "contato@richardsonlima.com.br"
+__status__ = "Test"
+
+import pycurl, json
+import os
+import platform
+import socket
+import time
+import psutil
+import json
+import urllib2
+
+disk = psutil.disk_usage('/')
+disk_total = disk.total / 2**30     # GiB.
+disk_used = disk.used / 2**30
+disk_free = disk.free / 2**30
+disk_percent_used = disk.percent
+cpu_usage = psutil.cpu_percent()
+#get_load = os.getloadavg()[0]
+#ram = psutil.phymem_usage()
+ram = psutil.virtual_memory() # psutil.phymem_usage() is deprecated
+ram_total = ram.total / 2**20       # MiB.
+ram_used = ram.used / 2**20
+ram_free = ram.free / 2**20
+ram_percent_used = ram.percent
+
+process = psutil.Process(os.getpid())
+print "[+] Platform: ",platform.system()
+print "[+] Hostname: ",socket.gethostname()
+print "[+] Memory percent used: ",ram_percent_used
+print "[+] CPU percent used: ",cpu_usage
+print "[+] Disk percent used: ",disk_percent_used
+#print "[+] Load: ",get_load
+
+api_url = 'http://api.domain:5000/api/v1/collector/add'
+
+data = json.dumps({"sistema": platform.system(), "hostname": socket.gethostname(), "percentual_memoria": ram_percent_used, "percentual_cpu": cpu_usage, "percentual_disco": disk_percent_used, "carga": "carga" })
+
+c = pycurl.Curl()
+c.setopt(pycurl.URL, api_url)
+c.setopt(pycurl.HTTPHEADER, ['Content-Type: application/json', 'Accept: application/json'])
+c.setopt(pycurl.POST, 1)
+c.setopt(pycurl.POSTFIELDS, data)
+c.perform()
